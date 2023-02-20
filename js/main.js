bba39a6d8064fa6a11887a4b1979ad441dc2d3b0
Your Name <you@example.com>
2023-02-20 16:00:28 +0100

    inserimento prompt

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const name = prompt("Please enter your name", " ");
+
+const surname = prompt("Please enter your surname", " ");
+
+const color = prompt("Please enter your favorite color", " ");
+
+const pass = prompt("This is your accuratissima and insgamabile password", " ");
