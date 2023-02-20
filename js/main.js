297921289289bd16fde01654f7510ee74aa1e06d
Your Name <you@example.com>
2023-02-20 16:23:48 +0100

    fine

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 'use strict';
 
-const name = prompt("Please enter your name", " ");
+const firstname = prompt("Please enter your name", " ");
+console.log(firstname)
 
 const surname = prompt("Please enter your surname", " ");
+console.log(surname)
 
 const color = prompt("Please enter your favorite color", " ");
+console.log(color)
 
-const pass = prompt("This is your accuratissima and insgamabile password", " ");
+const pass = `This is your accuratissima and insgamabile password: ${firstname}${surname}${color}89` ;
+console.log(pass)
+
+document.querySelector ('h1').innerHTML = pass;
